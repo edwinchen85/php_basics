9b06ce69def274dc52d280d1ce2f5de9751aa4d3
edwinchen85 <edwinchen85@gmail.com>
2017-06-18 16:35:51 +0800

    Integers and floats

---
+++ 03-data-types-integers-and-floats/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Integer
+$daysInWeek = 7;
+
+// Valid integer;
+$num = -5;
+$num = 0;
+
+// Float or floating point
+$pi = 3.141;
+
+// 1.2 millions using e
+$bankBalance = 1.2e6;
+
+echo 'Your bank balance is $' . $bankBalance;
