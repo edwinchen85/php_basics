d9c90edd05f2dc97dd7e9353d37deb90c8fb5577
Edwin Chen <edwinchen85@gmail.com>
2017-06-19 22:55:26 +0800

    Commenting

---
+++ 26-commenting/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Single line comment
+$name = 'alex';   // Able to comment inline
+
+
+/*
+  Block comment
+*/
+
+/*
+ * This function takes a string and strips the whitespace.
+ * More text here...
+*/
+function removedWhitespace() {
+
+}
